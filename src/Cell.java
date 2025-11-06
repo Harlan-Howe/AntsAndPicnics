3f51144e286cb23022318c94ee14c89f911b5122
Harlan Howe <harlan.howe@kinkaid.org>
2025-11-06 13:45:41 -0600

    Cells now can draw themselves.

--- src/Cell.java
+++ src/Cell.java
@@ -1,9 +1,15 @@
+import java.awt.*;
+
 public class Cell
 {
     public static final int TYPE_EMPTY = 0;
     public static final int TYPE_NEST = 1;
     public static final int TYPE_BASKET = 2;
     public static final int TYPE_ANT = 3;
+
+    public static final int LEFT_MARGIN = 10;
+    public static final int TOP_MARGIN = 10;
+    public static final int CELL_SIZE = 20;
 
     private int contentType;
     private int myRow, myCol;
@@ -28,7 +34,7 @@ public class Cell
     public Nest getNest()
     {
         if (contentType != TYPE_NEST)
-            throw new RuntimeException("You tried to get a nest at ("+myRow+", "+myCol+") but there isn't one there.");
+            throw new RuntimeException("You tried to get a nest at (" + myRow + ", " + myCol + ") but there isn't one there.");
         else
             return myNest;
     }
@@ -36,9 +42,17 @@ public class Cell
     public Basket getBasket()
     {
         if (contentType != TYPE_BASKET)
-            throw new RuntimeException("You tried to get a basket at ("+myRow+", "+myCol+") but there isn't one there.");
+            throw new RuntimeException("You tried to get a basket at (" + myRow + ", " + myCol + ") but there isn't one there.");
         else
             return myBasket;
     }
 
+    public void drawSelf(Graphics g)
+    {
+        g.setColor(new Color(myScents[0], myScents[1], myScents[2]));
+        g.fillRect(LEFT_MARGIN+myCol*CELL_SIZE, TOP_MARGIN+myRow*CELL_SIZE, CELL_SIZE, CELL_SIZE);
+        g.setColor(Color.darkGray);
+        g.drawRect(LEFT_MARGIN+myCol*CELL_SIZE, TOP_MARGIN+myRow*CELL_SIZE, CELL_SIZE, CELL_SIZE);
+
+    }
 }
