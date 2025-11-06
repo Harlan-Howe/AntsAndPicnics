42cabecd53c9070fb50133b2ae4eec50d7149712
Harlan Howe <harlan.howe@kinkaid.org>
2025-11-06 10:18:14 -0600

    wrote drawSelf() method

--- src/World.java
+++ src/World.java
@@ -1,3 +1,5 @@
+import java.awt.*;
+
 public class World
 {
     public static final int NUM_ROWS_IN_WORLD = 40;
@@ -12,5 +14,12 @@ public class World
             for (int c = 0; c<NUM_COLS_IN_WORLD; c++)
                 grid[r][c] = new Cell(r,c);
 
+    }
+
+    public void drawSelf(Graphics g)
+    {
+        for (int r =0; r < NUM_ROWS_IN_WORLD; r++)
+            for (int c = 0; c<NUM_COLS_IN_WORLD; c++)
+                grid[r][c].drawSelf(g);
     }
 }
