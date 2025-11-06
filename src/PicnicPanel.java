60d9d842ba579d6b713f17ea4cd7fecf2dd2f8f7
Harlan Howe <harlan.howe@kinkaid.org>
2025-11-06 13:50:43 -0600

    added public NEST_AND_ANT_COLORS

--- src/PicnicPanel.java
+++ src/PicnicPanel.java
@@ -3,16 +3,21 @@ import java.awt.*;
 
 public class PicnicPanel extends JPanel
 {
+
+    public static final Color[] NEST_AND_ANT_COLORS = {Color.RED, Color.GREEN, Color.BLUE};
+
     private World myWorld;
 
     public PicnicPanel(World w)
     {
         super();
         myWorld = w;
+        setBackground(Color.lightGray);
     }
 
     public void paintComponent(Graphics g)
     {
+        super.paintComponent(g);
         myWorld.drawSelf(g);
     }
 }
