9891b08b098f9e277e3c99fc6d5481ce296e9b2e
Harlan Howe <harlan.howe@kinkaid.org>
2025-11-06 16:28:29 -0600

    temp code in constructor to make demo nests and baskets wrote fadeAllScents()

--- src/World.java
+++ src/World.java
@@ -14,6 +14,15 @@ public class World
             for (int c = 0; c<NUM_COLS_IN_WORLD; c++)
                 grid[r][c] = new Cell(r,c);
 
+        Nest tempNest = new Nest(2, 2, 0);
+        grid[2][2].setNest(tempNest);
+
+        Basket tempBasket = new Basket();
+        grid[tempBasket.getRow()][tempBasket.getCol()].setBasket(tempBasket);
+        System.out.println(tempBasket.getRow()+","+tempBasket.getCol());
+
+        grid[20][20].markCellWithScent(2);
+
     }
 
     public void drawSelf(Graphics g)
@@ -22,4 +31,12 @@ public class World
             for (int c = 0; c<NUM_COLS_IN_WORLD; c++)
                 grid[r][c].drawSelf(g);
     }
+
+    public void fadeAllScents()
+    {
+        for (int r =0; r < NUM_ROWS_IN_WORLD; r++)
+            for (int c = 0; c<NUM_COLS_IN_WORLD; c++)
+                grid[r][c].fadeScents();
+    }
+
 }
