5bff03c4d0603a64ad37375a0f8ca9663d761757
Harlan Howe <harlan.howe@kinkaid.org>
2025-11-06 10:18:41 -0600

    Now tracks world and panel; constructor now does initial layout.

--- src/PicnicWindow.java
+++ src/PicnicWindow.java
@@ -1,10 +1,22 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class PicnicWindow extends JFrame
 {
+    private World myWorld;
+    private PicnicPanel myPanel;
+
     public PicnicWindow()
     {
         super("Picnic");
+        setSize(Cell.CELL_SIZE * World.NUM_COLS_IN_WORLD, Cell.CELL_SIZE * World.NUM_ROWS_IN_WORLD + 32);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.getContentPane().setLayout(new GridLayout(1,1));
+        myWorld = new World();
+        myPanel = new PicnicPanel(myWorld);
+        this.getContentPane().add(myPanel);
+        setResizable(false);
+        setVisible(true);
     }
 
 
