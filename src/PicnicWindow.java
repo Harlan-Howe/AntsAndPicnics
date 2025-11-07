67979eefa3cdbaed383ac39d94c19867f6edeb6e
Harlan Howe <harlan.howe@kinkaid.org>
2025-11-07 08:46:46 -0600

    documentation update

--- src/PicnicWindow.java
+++ src/PicnicWindow.java
@@ -11,11 +11,14 @@ public class PicnicWindow extends JFrame
         super("Picnic");
         setSize(Cell.CELL_SIZE * World.NUM_COLS_IN_WORLD+Cell.LEFT_MARGIN*2,
                 Cell.CELL_SIZE * World.NUM_ROWS_IN_WORLD + Cell.TOP_MARGIN* 2 + 32);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // quits program when window is closed.
+
+        // make a panel that fills the window.
         this.getContentPane().setLayout(new GridLayout(1,1));
         myWorld = new World();
         myPanel = new PicnicPanel(myWorld);
         this.getContentPane().add(myPanel);
+
         setResizable(false);
         setVisible(true);
     }
