eef30f07a1466afd15e198a0fe7985ad77b40291
Harlan Howe <harlan.howe@kinkaid.org>
2025-11-07 09:37:56 -0600

    documentation

--- src/Basket.java
+++ src/Basket.java
@@ -2,6 +2,8 @@ import java.awt.*;
 
 public class Basket
 {
+    // CONSTANTS
+    // set once (final) and shared with all the baskets (static)
     private static final int MAX_FOOD_LEVEL = 100;
     private static final int MIN_STARTING_FOOD_LEVEL = 50;
     private static final Color BROWN = new Color(64, 32, 0);
@@ -21,6 +23,11 @@ public class Basket
     public int getRow() { return myRow;}
     public int getCol() { return myCol;}
 
+    /**
+     * attempts to remove the given amount of food from this basket. If the basket doesn't have at least this amount of
+     * food, will throw an exception.
+     * @param amount - the amount of food to remove, which must be positive.
+     */
     public void takeFood(int amount)
     {
         if (amount < 1)
@@ -32,17 +39,27 @@ public class Basket
         myFoodLevel -= amount;
     }
 
+    /**
+     * resets this basket's position to a random location in the grid, WHICH MAY NOT BE EMPTY.
+     */
     public void pickRandomLocation()
     {
         myRow = (int)(World.NUM_ROWS_IN_WORLD*Math.random());
         myCol = (int)(World.NUM_COLS_IN_WORLD*Math.random());
     }
 
+    /**
+     * refills this basket with a random amount of food.
+     */
     public void pickStartingFoodLevel()
     {
         myFoodLevel = MIN_STARTING_FOOD_LEVEL + (int)((MAX_FOOD_LEVEL-MIN_STARTING_FOOD_LEVEL+1)*Math.random());
     }
 
+    /**
+     * drawst this basket in the cell
+     * @param g - the "Graphics" object that has the drawing tools and the canvas in which to draw.
+     */
     public void drawSelf(Graphics g)
     {
         int centerX = Cell.LEFT_MARGIN + Cell.CELL_SIZE*myCol + Cell.CELL_SIZE/2;
@@ -50,16 +67,19 @@ public class Basket
 
         g.setColor(BROWN);
 
+        // set up the quadrilateral that represents the body of the basket.
         int[] xPoints = {centerX-4*Cell.CELL_SIZE/10, centerX+4*Cell.CELL_SIZE/10, centerX+Cell.CELL_SIZE/5, centerX-Cell.CELL_SIZE/5};
         int[] yPoints = {centerY-Cell.CELL_SIZE/5, centerY-Cell.CELL_SIZE/5, centerY+4*Cell.CELL_SIZE/10, centerY+4*Cell.CELL_SIZE/10};
-
+        // draw the quadrilateral
         g.fillPolygon(xPoints, yPoints, 4);
-
+        // draw the handle
         g.fillRect(centerX-Cell.CELL_SIZE/10, centerY-4*Cell.CELL_SIZE/10, Cell.CELL_SIZE/5, Cell.CELL_SIZE/2);
 
+        // draw the amount of remaining food.
         String amount = ""+myFoodLevel;
         g.setFont(FOOD_FONT);
         int stringWidth = g.getFontMetrics().stringWidth(amount);
+        // we're drawing the number in black on a shifted white "shadow."
         g.setColor(Color.WHITE);
         g.drawString(amount, centerX-stringWidth/2+1, centerY+Cell.CELL_SIZE/5+1);
         g.setColor(Color.BLACK);
