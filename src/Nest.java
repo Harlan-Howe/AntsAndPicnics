00e2ed4ce30d6a20c7ab70386d35fff7c87cea22
Harlan Howe <harlan.howe@kinkaid.org>
2025-11-07 09:32:48 -0600

    documentation, including clarifying order of some lines.

--- src/Nest.java
+++ src/Nest.java
@@ -2,10 +2,14 @@ import java.awt.*;
 
 public class Nest
 {
+    // CONSTANTS
+    // this font will be set up once (final) and shared by all the nests (static).
+    private static final Font COUNTER_FONT = new Font(Font.SANS_SERIF, Font.PLAIN, 6);
+
     private int myRow, myCol;
     private int myColor;  // 0 = red, 1 = green, 2 = blue
     private int myTotalCollectedFood;
-    private static final Font COUNTER_FONT = new Font(Font.SANS_SERIF, Font.PLAIN, 6);
+
 
     public Nest(int row, int column, int color)
     {
@@ -40,17 +44,23 @@ public class Nest
         return myCol;
     }
 
+    /**
+     * draws this nest in its cell
+     * @param g - the "Graphics" object that has the drawing tools and canvas in which to draw.
+     */
     public void drawSelf(Graphics g)
     {
-        g.setColor(PicnicPanel.NEST_AND_ANT_COLORS[myColor]);
         int centerX = Cell.LEFT_MARGIN + myCol*Cell.CELL_SIZE + Cell.CELL_SIZE/2;
         int centerY = Cell.TOP_MARGIN + myRow*Cell.CELL_SIZE + Cell.CELL_SIZE/2;
 
+        // build the polygon for the triangle that represents the anthill
         int[] x_poly = {centerX-Cell.CELL_SIZE/3, centerX, centerX+Cell.CELL_SIZE/3};
         int[] y_poly = {centerY, centerY - 4*Cell.CELL_SIZE/10, centerY};
 
+        g.setColor(PicnicPanel.NEST_AND_ANT_COLORS[myColor]);
         g.fillPolygon(x_poly, y_poly, 3);
 
+        // draw the text with the contents of this anthill.
         g.setFont(COUNTER_FONT);
         String amount = ""+myTotalCollectedFood;
         int numberWidth = g.getFontMetrics().stringWidth(amount);
